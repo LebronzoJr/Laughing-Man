77c623505e97e20cb940f66c947ef36f71ea2df4
Lebron <alebron1290@gmail.com>
2026-05-10 14:45:51 -0400

    CreatedSquareForLoopAndAddedImg

--- laughing_man.js
+++ laughing_man.js
@@ -1,5 +1,20 @@
+const SQUARE_COUNT=3;
+
 document.addEventListener('DOMContentLoaded', ()=>{
     document.querySelector("#square").addEventListener("click",()=>{
         alert("OMG YOU CLICKED ME!");
-    })
+    });
+
+    let box = document.querySelector("#box");
+
+    for(let i =0;i < SQUARE_COUNT; i++){
+        //Makee the element, but its not anything or on the page
+        let square = document.createELement('img');
+        //Set the attributes/properties of that element.
+        square.src = "laughing_man.jpg";
+        square.alt = "Catch the Laughing Man!";
+        square.class = "square";
+        box.appendChild(square);
+        
+    }
 });
